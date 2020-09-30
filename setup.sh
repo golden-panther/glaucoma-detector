101aee39a404ca3f599c522d28df1b3b89be2859
Gnanendra Avvaru <32359487+golden-panther@users.noreply.github.com>
2020-09-30 16:26:30 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p ~/.streamlit/                                               
-echo "\                       
-[server]\n\                       
-port = $port\n\                       
-enableCORS = false\n\                       
-headless = true\n\                       
-\n\                       
-" > ~/.streamlit/config.toml
