842b32b19dc74b19d944990e672e8c0cfa8bcd32
Gnanendra Avvaru <32359487+golden-panther@users.noreply.github.com>
2020-09-30 11:14:31 +0530

    Add files via upload

---
+++ glaucoma_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+def import_and_predict(image_data, model):
+    image = ImageOps.fit(image_data, (100,100),Image.ANTIALIAS)
+    image = image.convert('RGB')
+    image = np.asarray(image)
+    st.image(image, channels='RGB')
+    image = (image.astype(np.float32) / 255.0)
+    img_reshape = image[np.newaxis,...]
+    prediction = model.predict(img_reshape)
+    return prediction
+
+
+model = tf.keras.models.load_model('my_model2.h5')
+
+st.write("""
+         # ***Glaucoma detector***
+         """
+         )
+
+st.write("This is a simple image classification web app to predict glaucoma through fundus image of eye")
+
+file = st.file_uploader("Please upload an image(jpg) file", type=["jpg"])
+
+if file is None:
+    st.text("You haven't uploaded a jpg image file")
+else:
+    imageI = Image.open(file)
+    prediction = import_and_predict(imageI, model)
+    pred = prediction[0][0]
+    if(pred > 0.5):
+        st.write("""
+                 ## **Prediction:** You eye is Healthy. Great!!
+                 """
+                 )
+    else:
+        st.write("""
+                 ## **Prediction:** You are affected by Glaucoma. Please consult an ophthalmologist as soon as possible.
+                 """
+                 )
